3b6a113e2ce59fb23079a88677fa2acc222f111f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-21 18:37:46 +0100

    Sync

---
+++ resources/views/proves.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	Proves
+@endsection
+
+@section('contentheader_title')
+	Proves 1
+@endsection
+
+@section('main-content')
+	<events-example></events-example>
+	<users></users>
+@endsection
