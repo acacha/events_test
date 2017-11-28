0565c7210fb493f74df5e76afa9cbbde2ad217f3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-28 14:49:48 +0100

    Sync

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Acacha\Events\Models\Traits\HasEvents;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
@@ -14,7 +15,7 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens,Notifiable, HasRoles;
+    use HasApiTokens,Notifiable, HasRoles, HasEvents;
 
     /**
      * The attributes that are mass assignable.
